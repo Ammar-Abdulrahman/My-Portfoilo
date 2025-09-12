a50313718972b03043cb9eb6d4a5affd9e5177c9
Ammar abdulrahman <Ammarabdulrahmman@gmail.com>
2025-09-12 14:52:40 +0300

    feat: handle global styles and structure files

---
+++ src/layout/index.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@Layout/Navbar";
+import Footer from "@Layout/Footer";
+
+const index = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+};
+
+export default index;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "@Styles/index.css"
+import App from "@Providers/App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(<App />);
